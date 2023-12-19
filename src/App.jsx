7610655b4339a20f0a3fcb8150f9b7db8babb387
Vanshul Agarwal <vanshulagarwal2004@gmail.com>
2023-12-20 02:17:48 +0530

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './App.css'
+import './App.scss'
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Products from './pages/Products/Products'
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,53 @@
 import React from "react"
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
 
 const Navbar = () => {
     return <>
-        Navbar
+        <div className="navbar">
+            <div className="wrapper">
+                <div className="left">
+                    <Link className="link" to="/">ECOMMERCE</Link>
+                </div>
+                <div className="center">
+                    <div className="item">
+                        <Link className="link" to="/">Home</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/products?categories=cate1">Cate1</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/products?categories=cate2">Cate2</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/products?categories=cate3">Cate3</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/products?categories=cate4">Cate4</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/">Contact</Link>
+                    </div>
+                </div>
+
+                <div className="right">
+                    <div className="icons">
+                        <SearchIcon />
+                        <PersonOutlineOutlinedIcon />
+                        <FavoriteBorderOutlinedIcon />
+                        <div className="cartIcon">
+                            <ShoppingBagOutlinedIcon />
+                            <span>0</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
 }
 
