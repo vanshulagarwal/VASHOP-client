edcd63c55749f10cb322e617a7b8ddc722916081
Vanshul Agarwal <vanshulagarwal2004@gmail.com>
2023-12-21 17:45:21 +0530

    completed home page

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Categories.scss";
+import { Link } from "react-router-dom";
+
+const Categories = () => {
+    return (
+        <div className="categories">
+            <div className="items cate1">
+                <img src="https://images.pexels.com/photos/157675/fashion-men-s-individuality-black-and-white-157675.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                <button>
+                    <Link to="/products" className="link">MEN</Link>
+                </button>
+            </div>
+            <div className="items cate2">
+                <img src="https://images.pexels.com/photos/19477597/pexels-photo-19477597/free-photo-of-young-brunette-standing-in-a-snowy-forest.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                <button>
+                    <Link to="/products" className="link">WOMEN</Link>
+                </button>
+            </div>
+            <div className="items cate3">
+                <img src="https://images.pexels.com/photos/336372/pexels-photo-336372.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                <button>
+                    <Link to="/products" className="link">SHOES</Link>
+                </button>
+            </div>
+            <div className="items cate4">
+                <img src="https://images.pexels.com/photos/2043590/pexels-photo-2043590.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                <button>
+                    <Link to="/products" className="link">WINTER</Link>
+                </button>
+            </div>
+            <div className="items cate5">
+                <img src="https://images.pexels.com/photos/325876/pexels-photo-325876.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                <button>
+                    <Link to="/products" className="link">FORMALS</Link>
+                </button>
+            </div>
+            <div className="items cate6">
+                <img src="https://images.pexels.com/photos/701877/pexels-photo-701877.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                <button>
+                    <Link to="/products" className="link">ACCESSORIES</Link>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Categories;
---
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./FeaturedProducts.scss";
+import Card from "../Card/Card";
+
+const FeaturedProducts = ({ type }) => {
+    const data = [
+        {
+            id: 1,
+            img1: "https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+            img2:"https://images.pexels.com/photos/878358/pexels-photo-878358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+            title: "Hoodie",
+            oldPrice: 1299,
+            price: 1099,
+        },
+        {
+            id: 2,
+            img1: "https://images.pexels.com/photos/428338/pexels-photo-428338.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+            img2:"https://images.pexels.com/photos/878358/pexels-photo-878358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+            title: "TShirt",
+            oldPrice: 999,
+            price: 699,
+        },
+        {
+            id: 3,
+            img1: "https://images.pexels.com/photos/18372435/pexels-photo-18372435/free-photo-of-light-landscape-sunset-fashion.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            img2:"https://images.pexels.com/photos/878358/pexels-photo-878358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+            title: "Hat",
+            oldPrice: 399,
+            price: 299,
+        },
+        {
+            id: 1,
+            img1: "https://images.pexels.com/photos/983564/pexels-photo-983564.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+            img2:"https://images.pexels.com/photos/878358/pexels-photo-878358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+            title: "Skirt",
+            oldPrice: 1699,
+            price: 1499,
+        },
+    ];
+
+    return (
+        <div className="featuredProducts">
+            <div className="top">
+                <h1>{type} Products</h1>
+            </div>
+            <div className="bottom">
+                {data.map(item => (
+                    <Card item={item} key={item.id} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default FeaturedProducts;
